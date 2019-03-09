c2bd3c0e75787c833d8fb01b1c7515be976ed83a
WE <WE@DESKTOP-0V8124L>
2019-03-09 10:24:10 +0800

    springboot对thymeleaf的支持

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String hello(Map<String,Object> map){
+		map.put("name", "张三");
+		return "hello";
+	}
+
+}
